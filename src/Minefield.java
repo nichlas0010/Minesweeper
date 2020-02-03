37c279fee6aa51ad15cf105f3dc773a17b87e943
Nichlas Pihl <nichlas0010@gmail.com>
2020-02-03 12:21:22 +0000

    adds a whole bunch of comments, split check for populate and toString()

--- src/Minefield.java
+++ src/Minefield.java
@@ -1,11 +1,23 @@
 import java.util.Random;
 
 public class Minefield {
+    // The actual minefield, true is mined, false is not mined
     private boolean[][] minefield;
+    // Amount of neighbours that are mined. Name is slightly misleading, since the number includes the tile itself
+    // It doesn't really matter though, since it will be represented as a * not the number if there's a mine
     private int[][] minedNeighbours;
+    // Maximum amount of mines, mineTile() will not exceed this value
     private int maxMines;
+    // Current amount of mines, incremented by mineTile()
     private int currentMines;
 
+    /**
+     * Instantiates all instance variables.
+     *
+     * @param rows amount of rows in the minefield, should be more than 0
+     * @param columns amount of columns in the minefield, should be more than 0
+     * @param maxMines maximum amount of mines, should not exceed (rows * colums - 1)
+     */
     public Minefield(int rows, int columns, int maxMines) {
         if(rows < 1 || columns < 1) {
             System.out.println("ERROR: Minefield must be at least 1x1.");
@@ -21,11 +33,27 @@ public class Minefield {
         currentMines = 0;
     }
 
+    /**
+     * Method to mine a tile at given co-ordinate (row, column), and increase minedNeighbours of neighbouring tiles.
+     * Will fail if row/column is larger/smaller than the grid's dimensions, if the maxMines limit has been reached,
+     * or if the tile has already been mined.
+     *
+     * @param row row number to insert mine at
+     * @param column column number to insert mine at
+     * @return true if tile was mined successfully, false if mining failed
+     */
     public boolean mineTile(int row, int column) {
+        // These two if-statements have been split for readibility purposes
+        // This check makes sure we're not exceeding the bouns of the minefield, in either direction
+        if(row < 0 || row >= minefield.length || column < 0 || column >= minefield[0].length) {
+            return false;
+        }
+        // This check makes sure the tile has not aready been mined, and that we have spare mines.
         if(minefield[row][column] || currentMines >= maxMines) {
             return false;
         }
         minefield[row][column] = true;
+        // Iterate through the neighbouring tiles (including our own), and increment each
         for(int i = row-1; i <= row+1; i++) {
             for(int j = column-1; j <= column+1; j++) {
                 if(i >= 0 && i < minefield.length && j >= 0 && j < minefield[0].length) {
@@ -33,26 +61,42 @@ public class Minefield {
                 }
             }
         }
+        // Increment currentMines
         currentMines++;
         return true;
 
     }
 
+    /**
+     * Populates the minefield, up to the maxMines passed to the constructor. Doesn't mine (0, 0).
+     */
     public void populate() {
         Random random = new Random();
-        int minefieldSize = minefield.length*minefield[0].length - 1;
-        while(currentMines < maxMines) {
-            int randomPosition = random.nextInt(minefieldSize) + 1;
-            minefieldSize--;
-            int h = 0;
-            main: for(int i = 0; i < minefield.length; i++) {
+        // Determine the amount of free tiles, excluding (0,0)
+        int freeTiles = -1;
+        for(int i = 0; i < minefield.length; i++) {
+            for(int j = 0; j < minefield.length; j++) {
+                if(!minefield[i][j]) {
+                    freeTiles++;
+                }
+            }
+        }
+        main: while(currentMines < maxMines) {
+            // Pick a random position between 0 and our amount of free tiles, then increment by one to avoid (0,0)
+            int randomPosition = random.nextInt(freeTiles) + 1;
+            freeTiles--;
+            int currentPosition = 0;
+            // Iterate through the minefield to find the randomPositionth free tile, then mine it
+            for(int i = 0; i < minefield.length; i++) {
                 for(int j = 0; j < minefield[0].length; j++) {
+                    // We only care about free tiles
                     if(!minefield[i][j]) {
-                        if(h < randomPosition) {
+                        if(currentPosition < randomPosition) {
                             h++;
                         } else {
                             if(mineTile(i, j)) {
-                                break main;
+                                // Continue the while-loop
+                                continue main;
                             }
                         }
                     }
@@ -61,6 +105,12 @@ public class Minefield {
         }
     }
 
+    /**
+     * Returns a string consisting of the entire minefield. Mined tiles will be represented by "*", free tiles will be
+     * represented by the amount of neighbouring tiles that are mined. Rows are seperated by a newline.
+     *
+     * @return String consisting of the entire minefield.
+     */
     public String toString() {
         String output = "";
         for(int i = 0; i < minefield.length; i++) {
@@ -77,11 +127,25 @@ public class Minefield {
         return output;
     }
 
+    /**
+     * @return the 2d boolean array of mines
+     */
     public boolean[][] getMinefield() {
         return minefield;
     }
 
+    /**
+     * @return the 2d int array of mined neighbours
+     */
     public int[][] getMinedNeighbours() {
         return minedNeighbours;
+    }
+
+    public int getCurrentMines() {
+        return currentMines;
+    }
+
+    public int getMaxMines() {
+        return maxMines;
     }
 }
--- src/MinefieldTest.java
+++ src/MinefieldTest.java
@@ -8,19 +8,61 @@ public class MinefieldTest {
     public void checkMinefieldSize() {
         for(int i = 1; i <= 10; i++) {
             for(int j = 1; j <= 10; j++) {
-            Minefield M = new Minefield(i, j, 0);
+            Minefield M = new Minefield(i, j, 5);
             boolean[][] mineField = M.getMinefield();
             assertEquals(i*j, mineField.length*mineField[0].length);
             int[][] minedNeighbours = M.getMinedNeighbours();
             assertEquals(i*j, minedNeighbours.length*minedNeighbours[0].length);
+            assertEquals(5, M.getMaxMines());
             }
         }
 
     }
 
-    // Check for populate() and toString()
+    // Check for populate()
     @Test
-    public void checkAmountOfMines() {
+    public void checkpopulate() {
+        for(int i = 1; i <= 10; i++) {
+            for(int j = 1; j <= 10; j++) {
+                for(int k = 1; k <= i*j-1; k++) {
+                    Minefield M = new Minefield(i, j, k);
+                    M.populate();
+                    assertEquals(k, M.getCurrentMines());
+                    boolean[][] minefield = M.getMinefield();
+                    int[][] minedNeighbours = M.getMinedNeighbours();
+                    for(int l = 0; l <= minefield.length; l++) {
+                        for(int m = 0; m <= minefield[0].length; m++) {
+                            int q = 0;
+                            for(int o = l-1; o <= l+1; o++) {
+                                for(int p = m-1; p <= m+1; p++) {
+                                    if(i >= 0 && i < minefield.length && j >= 0 && j < minefield[0].length) {
+                                        if(minefield[o][p]) {
+                                            q++;
+                                        }
+                                    }
+                                }
+                            }
+                            assertEquals(minedNeighbours[l][m], q);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // Check for mineTile()
+    @Test
+    public void checkMineTile() {
+        Minefield M = new Minefield(2, 2, 2);
+        assertEquals(true, M.mineTile(1, 0));
+        assertEquals(false, M.mineTile(1, 0));
+        assertEquals(true, M.mineTile(1, 1));
+        assertEquals(false, M.mineTile(0, 1));
+    }
+
+    // Check for toString()
+    @Test
+    public void checkpopulate() {
         for(int i = 1; i <= 10; i++) {
             for(int j = 1; j <= 10; j++) {
                 for(int k = 1; k <= i*j-1; k++) {
@@ -37,15 +79,5 @@ public class MinefieldTest {
                 }
             }
         }
-    }
-    
-    // Check for mineTile()
-    @Test
-    public void checkMineTile() {
-        Minefield M = new Minefield(2, 2, 2);
-        assertEquals(true, M.mineTile(1, 0));
-        assertEquals(false, M.mineTile(1, 0));
-        assertEquals(true, M.mineTile(1, 1));
-        assertEquals(false, M.mineTile(0, 1));
     }
 }
